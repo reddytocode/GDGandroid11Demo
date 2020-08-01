5ebe6bd2aa59323d80fd57bcbeab7f0d0750729a
Reddy Tintaya <aaabeeelooon@gmail.com>
2020-08-01 02:59:35 -0400

    camera X basic configuration

---
+++ app/src/main/java/com/reddy/demo/ui/main/MainFragment.kt
@@ -0,0 +1,156 @@
+package com.reddy.demo.ui.main
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import com.reddy.demo.R
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import kotlin.math.abs
+import kotlin.math.ln
+import kotlin.math.max
+import kotlin.math.min
+
+class MainFragment : Fragment() {
+    private val viewModel: MainViewModel by viewModels()
+
+    private var displayId: Int = -1
+    private var camera: Camera? = null
+    private lateinit var container: ConstraintLayout
+    private lateinit var previewView: PreviewView
+
+    private lateinit var cameraExecutor: ExecutorService
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        container = view as ConstraintLayout
+        previewView = container.findViewById(R.id.preview)
+
+        cameraExecutor = Executors.newCachedThreadPool()
+        viewModel.executor = cameraExecutor
+
+        if (allPermissionsGranted()) {
+            previewView.post {
+                displayId = previewView.display.displayId
+                setUpCamera()
+            }
+        } else {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+    }
+
+    private fun setUpCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = try {
+                cameraProviderFuture.get()
+            } catch (e: ExecutionException) {
+                throw IllegalStateException("Camera initialization failed.", e.cause!!)
+            }
+            bindCameraUseCases(cameraProvider)
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun bindCameraUseCases(cameraProvider: ProcessCameraProvider) {
+
+        val metrics = DisplayMetrics().also { previewView.display.getRealMetrics(it) }
+//        Log.d(TAG, "Screen metrics: ${metrics.widthPixels} x ${metrics.heightPixels}")
+
+        val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
+//        Log.d(TAG, "Preview aspect ratio: $screenAspectRatio")
+
+        val rotation = previewView.display.rotation
+
+        val preview = Preview.Builder()
+                .setTargetAspectRatio(screenAspectRatio)
+                .setTargetRotation(rotation)
+                .build()
+
+//        viewModel.sourceText.observe(viewLifecycleOwner, Observer { srcText.text = it })
+//        viewModel.imageCropPercentages.observe(viewLifecycleOwner,
+//                Observer { drawOverlay(overlay.holder, it.first, it.second) })
+
+        val cameraSelector =
+                CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+        try {
+            // Unbind use cases before rebinding
+            cameraProvider.unbindAll()
+
+            // Bind use cases to camera
+            camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            preview.setSurfaceProvider(previewView.createSurfaceProvider())
+        } catch (exc: IllegalStateException) {
+            Log.e(TAG, "Use case binding failed. This must be running on main thread.", exc)
+        }
+    }
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = ln(max(width, height).toDouble() / min(width, height))
+        if (abs(previewRatio - ln(RATIO_4_3_VALUE))
+                <= abs(previewRatio - ln(RATIO_16_9_VALUE))
+        ) {
+            return AspectRatio.RATIO_4_3
+        }
+        return AspectRatio.RATIO_16_9
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                previewView.post { setUpCamera() }
+            } else {
+                Toast.makeText(
+                        context,
+                        "Permissions not granted by the user.",
+                        Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+                requireContext(), it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        fun newInstance() = MainFragment()
+
+        const val DESIRED_WIDTH_CROP_PERCENT = 8
+        const val DESIRED_HEIGHT_CROP_PERCENT = 74
+
+        private const val REQUEST_CODE_PERMISSIONS = 10
+
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val RATIO_4_3_VALUE = 4.0 / 3.0
+        private const val RATIO_16_9_VALUE = 16.0 / 9.0
+        private const val TAG = "MainFragment"
+    }
+
+}
---
+++ app/src/main/java/com/reddy/demo/ui/main/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.reddy.demo.ui.main
+
+import androidx.lifecycle.ViewModel
+import java.util.concurrent.Executor
+
+class MainViewModel : ViewModel() {
+    lateinit var executor: Executor
+
+}
